e89b0d64158c5030d7b2f8acd23e51baabc197f7
Inderbir Singh <inderbir@pluginhive.com>
2026-02-28 18:50:58 +0530

    done till save in packaging

--- src/pages/settings.ts
+++ src/pages/settings.ts
@@ -5,22 +5,26 @@ export class SettingsPage {
 
   //Locators
   readonly parcelPackingDropdown: Locator;
+  readonly parcelPackingDropdownOptions: Locator;
+  readonly saveChangesButtonInPackaging: Locator;
 
   constructor(page: Page) {
     this.page = page;
 
     //Locators
-    this.parcelPackingDropdown = this.page.locator('#select2-woocommerce_wf_shipping_ups_packing_method-container');
+    this.parcelPackingDropdown = this.page.locator('#select2-woocommerce_wf_shipping_ups_packing_method-container').first();
+    this.parcelPackingDropdownOptions = this.page.locator('.select2-container--open .select2-results__option');
+    this.saveChangesButtonInPackaging = this.page.getByRole('button', { name: 'save' });
   }
 
   async selectTab(tabName: string) {
+    await this.page.waitForLoadState('domcontentloaded');
     await this.page.getByText(tabName, { exact: true }).click();
   }
 
   async selectParcelPackingOption(optionName: string) {
     await this.parcelPackingDropdown.click();
-    const option = this.page.locator('.select2-container--open .select2-results__option').filter({ hasText: optionName });
-    await expect(option).toBeVisible();
+    const option = this.parcelPackingDropdownOptions.filter({ hasText: optionName });
     await option.first().click();
   }
 }
--- tests/singleLabeGeneration.spec.ts
+++ tests/singleLabeGeneration.spec.ts
@@ -1,12 +1,11 @@
-import { test, expect } from '/home/inderbir-singh/Documents/ups-woo-automation/tests/fixtures.ts';
+import { test, expect } from './fixtures.ts';
 
-test('has title', async ({ page, homePage, settingsPage }) => {
+test('Single Label Generation', async ({ page, homePage, settingsPage }) => {
   await homePage.goto();
   await homePage.UPSplugin.click();
   await homePage.settingsInUPSplugin.click();
-  await page.waitForLoadState('domcontentloaded');
   await settingsPage.selectTab('Packaging');
-  await settingsPage.parcelPackingDropdown.click();
   await settingsPage.selectParcelPackingOption('Default: Pack items individually');
-  await page.waitForTimeout(3000);
+  await expect(settingsPage.parcelPackingDropdown).toContainText('Default: Pack items individually');
+  await settingsPage.saveChangesButtonInPackaging.click();
 });
