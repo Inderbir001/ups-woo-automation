74d9c5e159066affb1cfaa67b3cb252fb5db970d
Inderbir Singh <inderbir@pluginhive.com>
2026-03-01 12:50:29 +0530

    fixed bug

--- tests/auth/auth.spec.ts
+++ tests/auth/auth.spec.ts
@@ -6,11 +6,11 @@ const authFile = path.join(__dirname, '../playwright/.auth/user.json');
 
 setup('authenticate', async ({ page }) => {
   const loginPage = new LoginPage(page);
-  await page.goto(`${process.env.site_url!}'/wp-admin/'`);
+  await page.goto(`${process.env.site_url!}/wp-admin/`);
   await loginPage.userName.fill(process.env.userName!);
   await loginPage.userPass.fill(process.env.pass!);
   await loginPage.loginBtn.click();
   await page.waitForLoadState('load');
-  await page.waitForURL(`${process.env.site_url!}'/wp-admin/'`);
+  await page.waitForURL(`${process.env.site_url!}/wp-admin/`);
   await page.context().storageState({ path: authFile });
 });
