2181c344648a6aa7d9c09604ceba30a686023bd6
Inderbir Singh <inderbir@pluginhive.com>
2026-05-26 17:30:52 +0530

    uncommented auth

--- tests/auth/auth.spec.ts
+++ tests/auth/auth.spec.ts
@@ -1,18 +1,18 @@
-// import { test as setup, expect } from '@playwright/test';
-// import path from 'path';
-// import { LoginPage } from '../../src/pages/auth/loginPage';
+import { test as setup, expect } from '@playwright/test';
+import path from 'path';
+import { LoginPage } from '../../src/pages/auth/loginPage';
 
-// const authFile = path.join(__dirname, '../../playwright/.auth/user.json');
+const authFile = path.join(__dirname, '../../playwright/.auth/user.json');
 
-// setup.use({ storageState: { cookies: [], origins: [] } });
+setup.use({ storageState: { cookies: [], origins: [] } });
 
-// setup('authenticate', async ({ page }) => {
-//   const loginPage = new LoginPage(page);
-//   await page.goto(`${process.env.site_url!}/wp-admin/`);
-//   await loginPage.userName.fill(process.env.userName!);
-//   await loginPage.userPass.fill(process.env.pass!);
-//   await loginPage.loginBtn.click();
-//   await page.waitForLoadState('load');
-//   await page.waitForURL(`${process.env.site_url!}/wp-admin/`);
-//   await page.context().storageState({ path: './playwright/.auth/user.json' });
-// });
+setup('authenticate', async ({ page }) => {
+  const loginPage = new LoginPage(page);
+  await page.goto(`${process.env.site_url!}/wp-admin/`);
+  await loginPage.userName.fill(process.env.userName!);
+  await loginPage.userPass.fill(process.env.pass!);
+  await loginPage.loginBtn.click();
+  await page.waitForLoadState('load');
+  await page.waitForURL(`${process.env.site_url!}/wp-admin/`);
+  await page.context().storageState({ path: './playwright/.auth/user.json' });
+});
